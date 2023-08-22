ec8fdd8ac8938abf45e72553bbcb8c5591a584e9
jon <jon6566@gmail.com>
2023-08-22 14:11:21 -0700

    .

---
+++ forms.py
@@ -0,0 +1,39 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class MessageForm(FlaskForm):
+    """Form for adding/editing messages."""
+
+    text = TextAreaField("text", validators=[DataRequired()])
+
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[Length(min=6)])
+    image_url = StringField("(Optional) Image URL")
+    location = StringField("Location")
+    bio = TextAreaField("Bio")
+    header_image_url = StringField("Header Image URL")
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[Length(min=6)])
+
+
+class ProfileForm(FlaskForm):
+    """Edit profile form"""
+
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    image_url = StringField("(Optional) Image URL")
+    header_image_url = StringField("Header Image URL")
+    bio = TextAreaField("Bio")
+    password = PasswordField("Password", validators=[Length(min=6)])
